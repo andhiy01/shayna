e0ae730ea307fa4cc4913796225a1a69c57ef252
andhiy01 <andipratama8167@gmail.com>
2021-04-28 11:35:21 +0700

    upload project shaynavue

---
+++ src/components/BannerShayna.vue
@@ -0,0 +1,113 @@
+<template>
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :nav='false' :dots="false" :items="3" :autoplay="true">
+                        <div class="product-item">
+                            <div class="pi-pic mx-1">
+                                <img src="img/mickey1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Bag</div>
+                                <a href="#">
+                                    <h5>Mickey Baggy</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic mx-1">
+                                <img src="img/products/women-2.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Cardigan</div>
+                                <a href="#">
+                                    <h5>Pinky Love</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic mx-1">
+                                <img src="img/products/women-1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Cardigan</div>
+                                <a href="#">
+                                    <h5>Yellow Moon</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic mx-1">
+                                <img src="img/products/women-3.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Cardigan</div>
+                                <a href="#">
+                                    <h5>Dust Grey</h5>
+                                </a>
+                                <div class="product-price">
+                                    $14.00
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+
+import carousel from 'vue-owl-carousel'
+export default {
+    name: 'BannerShayna',
+    components: {
+        carousel
+    }
+}
+</script>
+
+<style scoped>
+.pi-pic img{
+    height: 380px;
+}
+</style>
---
+++ src/components/SliderShayna.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="hero-section">
+        <carousel class="hero-items" :autoplay="true" :items="1" :nav="false" :dots="true">
+            <div class="single-hero-items p-0" style="background-image:url('img/hero-1.jpg');background-size:100%;background-repeat: no-repeat;">
+                <div class="container py-5" >
+                    <div class="row py-5">
+                        <div class="col-lg-5 text-left">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-hero-items p-0" style="background-image:url('img/hero-2.jpg');background-size:100%;background-repeat: no-repeat;" >
+                <div class="container py-5" style="background:">
+                    <div class="row py-5">
+                        <div class="col-lg-5 text-left">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </carousel>
+    </section>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+export default {
+    name: 'SliderShayna',
+    components:{
+        carousel,
+    }
+}
+</script>
+
+
+<style scoped>
+    .single-hero-items{
+        max-height: 500px
+    }
+</style>
